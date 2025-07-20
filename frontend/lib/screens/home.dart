9a2fcdd9fdc10dd356111e06f61f270443ce05ea
amyyong11 <amy.yong11@yahoo.com>
2025-07-19 22:09:39 -0400

    Added popup menu for creating a new media post or moodboard

--- frontend/lib/screens/home.dart
+++ frontend/lib/screens/home.dart
@@ -1,4 +1,3 @@
-// lib/screens/home_screen.dart
 import 'package:flutter/material.dart';
 import '../widgets/moodboard_card.dart';
 
@@ -37,6 +36,52 @@ class HomeScreen extends StatelessWidget {
       'artist': 'Echo Bloom'
     },
   ];
+
+  void _showCreateModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      backgroundColor: const Color(0xFF2B2B2B),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
+      builder: (_) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Create a new…',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _ModalOption(
+                    icon: Icons.post_add,
+                    label: 'Media Post',
+                    onTap: () {
+                      Navigator.pop(context);
+                      // TODO: Navigate to Media Post screen
+                    },
+                  ),
+                  _ModalOption(
+                    icon: Icons.grid_view,
+                    label: 'Moodboard',
+                    onTap: () {
+                      Navigator.pop(context);
+                      // TODO: Navigate to Moodboard screen
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,12 +122,50 @@ class HomeScreen extends StatelessWidget {
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white38,
         type: BottomNavigationBarType.fixed,
+        onTap: (index) {
+          if (index == 2) {
+            _showCreateModal(context); // Show modal when "+" is tapped
+          }
+        },
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.add_box), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.notifications_none), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: ''),
+        ],
+      ),
+    );
+  }
+}
+
+class _ModalOption extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const _ModalOption({
+    required this.icon,
+    required this.label,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 184, 117, 219),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.all(20),
+            child: Icon(icon, size: 28, color: Colors.black),
+          ),
+          const SizedBox(height: 8),
+          Text(label, style: const TextStyle(color: Colors.white)),
         ],
       ),
     );
